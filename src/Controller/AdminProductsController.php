f0608927765022b44e51559c342691fbd82633ae
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-04 07:47:53 +0200

    KR - Création du controller

---
+++ src/Controller/AdminProductsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminProductsController extends AbstractController
+{
+    #[Route('/admin/products', name: 'app_admin_products')]
+    public function index(): Response
+    {
+        return $this->render('admin_products/index.html.twig', [
+            'controller_name' => 'AdminProductsController',
+        ]);
+    }
+}
