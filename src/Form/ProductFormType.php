52dc5d750df7e559f714337b25f8205c0942cdf4
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-04 20:55:33 +0200

    KR - Création du formulaire de produits

---
+++ src/Form/ProductFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Products;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('product_name')
+            ->add('product_desc')
+            ->add('product_shop_url')
+            ->add('product_doc_url')
+            ->add('product_colors')
+            ->add('product_accessories')
+            ->add('product_long_desc')
+            ->add('product_carac')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
