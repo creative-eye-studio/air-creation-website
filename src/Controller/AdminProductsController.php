75015a744ed14698779d89d86ef487ac20726b5e
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-05 06:19:51 +0200

    KR - Intégration du formulaire "Ajouter un produit"

--- src/Controller/AdminProductsController.php
+++ src/Controller/AdminProductsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ProductFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,9 +19,17 @@ class AdminProductsController extends AbstractController
     }
 
     #[Route('/admin/products/ajouter', name: 'app_admin_products_add')]
-    public function AddProduct(): Response
+    public function AddProduct(Request $request): Response
     {
+        $form = $this->createForm(ProductFormType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            
+        }
+
         return $this->render('admin_products/add-product.html.twig', [
+            'form' => $form->createView(),
             'controller_name' => 'AdminProductsController',
         ]);
     }
