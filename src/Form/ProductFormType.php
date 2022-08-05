5ba4d258a374d778353f457f483af182153398fd
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-05 08:11:40 +0200

    KR - Configuration du formulaire "Ajouter un produit"

--- src/Form/ProductFormType.php
+++ src/Form/ProductFormType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Products;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +17,27 @@ class ProductFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('product_name')
-            ->add('product_desc')
-            ->add('product_shop_url')
-            ->add('product_doc_url')
-            ->add('product_colors')
-            ->add('product_accessories')
-            ->add('product_long_desc')
-            ->add('product_carac')
-            ->add('product_submit')
+            ->add('product_name', TextType::class, [
+                'label' => 'Nom du produit' 
+            ])
+            ->add('product_desc', CKEditorType::class, [
+                'label' => 'Présentation du produit'
+            ])
+            ->add('product_shop_url', UrlType::class, [
+                'label' => 'Lien vers la page E-Commerce'
+            ])
+            ->add('product_doc_url', UrlType::class, [
+                'label' => 'Lien vers la documentation'
+            ])
+            ->add('product_long_desc', CKEditorType::class, [
+                'label' => 'Présentation du produit'
+            ])
+            ->add('product_carac', CKEditorType::class, [
+                'label' => 'Caractéristiques du produit (Utiliser le tableau pour la mise en page)',
+            ])
+            ->add('product_submit', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
         ;
     }
 
