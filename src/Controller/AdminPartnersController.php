d9b0feceff95080b7ffd3ed3b12225c790f4d343
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-08 07:31:17 +0200

    KR - Intégration du formulaire "Ajouter un partenaire"

--- src/Controller/AdminPartnersController.php
+++ src/Controller/AdminPartnersController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\PartnersType;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +15,18 @@ class AdminPartnersController extends AbstractController
     public function index(): Response
     {
         return $this->render('admin_partners/index.html.twig', [
+            'controller_name' => 'AdminPartnersController',
+        ]);
+    }
+
+    #[Route('/admin/partners/ajouter', name: 'app_admin_partners_add')]
+    public function add_partner(Request $request)
+    {
+        $form = $this->createForm(PartnersType::class);
+        // $form->handleRequest($request);
+
+        return $this->render('admin_partners/add-partner.html.twig', [
+            'form' => $form->createView(),
             'controller_name' => 'AdminPartnersController',
         ]);
     }
