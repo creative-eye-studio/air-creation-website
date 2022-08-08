19ce2b35c96913d697073061b1ae4da56cb69160
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-08 07:54:37 +0200

    KR - Mise en page du formulaire "Ajouter un partenaire"

--- src/Form/PartnersType.php
+++ src/Form/PartnersType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Partners;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +17,40 @@ class PartnersType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('partner_cat')
-            ->add('adress')
-            ->add('postcode')
-            ->add('city')
-            ->add('website')
-            ->add('coord_long')
-            ->add('coord_lat')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la société'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email de la société'
+            ])
+            ->add('partner_cat', ChoiceType::class, [
+                'choices'=> [
+                    'Revendeur' => 0,
+                    'Formateur' => 1,
+                ],
+                'label' => "Catégorie du partenaire"
+            ])
+            ->add('adress', TextType::class, [
+                'label' => "Adresse postale"
+            ])
+            ->add('postcode', TextType::class, [
+                'label' => "Code postal"
+            ])
+            ->add('city', TextType::class, [
+                'label' => "Ville"
+            ])
+            ->add('website', UrlType::class, [
+                'label' => "Site Internet"
+            ])
+            ->add('coord_long', TextType::class, [
+                'label' => "Coordonnées Longitude"
+            ])
+            ->add('coord_lat', TextType::class, [
+                'label' => "Coordonnées Latitude"
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Envoyer"
+            ])
         ;
     }
 
