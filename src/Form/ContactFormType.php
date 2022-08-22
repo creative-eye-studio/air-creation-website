5920fe879c3ec127a988722d93eb2ba186737094
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-22 07:38:31 +0200

    KR - Création du formulaire de contact

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -3,6 +3,12 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +17,68 @@ class ContactFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('gender', ChoiceType::class, [
+                'choices'  => [
+                    'Madame / Monsieur' => null,
+                    'Monsieur' => 1,
+                    'Madame' => 2,
+                ],
+                'label' => null
+            ])
+            ->add('lname', TextType::class, [
+                'label' => null,
+                'attr' => [
+                    'placeholder' => 'Nom',
+                    'aria-label' => 'Nom'
+                ]
+            ])
+            ->add('fname', TextType::class, [
+                'label' => null,
+                'attr' => [
+                    'placeholder' => 'Prénom',
+                    'aria-label' => 'Prénom'
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'label' => null,
+                'attr' => [
+                    'placeholder' => 'Téléphone',
+                    'aria-label' => 'Téléphone'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => null,
+                'attr' => [
+                    'placeholder' => 'Adresse E-Mail',
+                    'aria-label' => 'Adresse E-Mail'
+                ]
+            ])
+            ->add('customer_type', ChoiceType::class, [
+                'choices'  => [
+                    'Client' => 'Client',
+                    'Distributeur France' => 'Distributeur France',
+                    'Distributeur International' => 'Distributeur International',
+                ],
+                'label' => 'Vous êtes',
+                'expanded' => true
+            ])
+            ->add('subject', ChoiceType::class, [
+                'choices' => [
+                    'Catégorie de la demande' => null,
+                    'Contacter le service commercial' => 1,
+                    'Déposer ma candidature' => 2,
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => null,
+                'attr' => [
+                    'placeholder' => 'Détails de la demande',
+                    'aria-label' => 'Détails de la demande'
+                ]
+            ])
+            ->add('submit_button', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
         ;
     }
 
