5492ae6a8f3517f12678f3bff21846720aabd3b8
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-24 21:59:06 +0200

    Update WebContactPageController.php

--- src/Controller/WebContactPageController.php
+++ src/Controller/WebContactPageController.php
@@ -14,8 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class WebContactPageController extends AbstractController
 {
     #[Route('/fr/contact', name: 'web_contact_page')]
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (New TemplatedEmail())
+                ->from($data['email'])
+                ->to('contact@aircreation.fr')
+                ->subject($data['subject'])
                 ->htmlTemplate('')
                 ->context([
                     'gender' => $data['gender'],
