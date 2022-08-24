e4a606ed36eb64c4b378fa0661f3e7c40fbc447b
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-08-24 21:58:19 +0200

    KR - Configuration du formulaire de contact

--- src/Controller/WebContactPageController.php
+++ src/Controller/WebContactPageController.php
@@ -3,16 +3,33 @@
 namespace App\Controller;
 
 use App\Form\ContactFormType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WebContactPageController extends AbstractController
 {
     #[Route('/fr/contact', name: 'web_contact_page')]
-    public function index(): Response
     {
         $form = $this->createForm(ContactFormType::class);
+                ->htmlTemplate('')
+                ->context([
+                    'gender' => $data['gender'],
+                    'lname' => $data['lname'],
+                    'fname' => $data['fname'],
+                    'phone' => $data['phone'],
+                    'email' => $data['email'],
+                    'customer_type' => $data['customer_type'],
+                    'message' => $data['message'],
+                ]);
+
+            $mailer->send($email);
+            return $this->redirectToRoute('web_contact_page');
+        }
 
         return $this->render('web_contact_page/index.html.twig', [
             'controller_name' => 'WebContactPageController',
