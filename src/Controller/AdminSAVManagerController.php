a68ad7aadcfde95db6e36bcda9455a3f89faa12a
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-09-04 22:11:48 +0200

    KR - Création du formulaire

--- src/Controller/AdminSAVManagerController.php
+++ src/Controller/AdminSAVManagerController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\FAQManagerFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +16,19 @@ class AdminSAVManagerController extends AbstractController
         return $this->render('admin_sav_manager/index.html.twig', [
             'controller_name' => 'AdminSAVManagerController',
         ]);
+    }
+
+    #[Route('/admin/sav-manager/ajouter-question', name: 'app_admin_sav_manager_add_question')]
+    public function add_question(Request $request) 
+    {
+
+        $form = $this->createForm(FAQManagerFormType::class);
+        $form->handleRequest($request);
+
+        return $this->render('admin_sav_manager/add-question.html.twig', [
+            'form' => $form->createView(),
+            'controller_name' => 'AdminSAVManagerController',
+        ]);
+        
     }
 }
