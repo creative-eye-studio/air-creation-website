b0fee8649e9de62b8528210bd46156a4f3f83bea
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-09-05 06:48:58 +0200

    KR - Management des FAQ

--- src/Controller/AdminSAVManagerController.php
+++ src/Controller/AdminSAVManagerController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\FAQList;
 use App\Form\FAQManagerFormType;
+use Cocur\Slugify\Slugify;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,25 +13,140 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminSAVManagerController extends AbstractController
 {
+
+    /* PAGE SAV
+    ------------------------------------------------------- */
     #[Route('/admin/sav-manager', name: 'app_admin_sav_manager')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
+        $faqList = $entityManager->getRepository(FAQList::class)->findAll();
+
         return $this->render('admin_sav_manager/index.html.twig', [
             'controller_name' => 'AdminSAVManagerController',
+            'faqList' => $faqList,
         ]);
     }
 
+    /* AJOUTER UNE QUESTION
+    ------------------------------------------------------- */
     #[Route('/admin/sav-manager/ajouter-question', name: 'app_admin_sav_manager_add_question')]
-    public function add_question(Request $request) 
+    public function add_question(ManagerRegistry $doctrine, Request $request) 
     {
 
         $form = $this->createForm(FAQManagerFormType::class);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slugify = new Slugify();
+
+            // Récupération des données du form
+            $data = $form->getData();
+            $faqName = $data['faq_name'];
+            $faqContent = $data['faq_content'];
+            $faqId = $slugify->slugify($faqName);
+            $faqId = $slugify->slugify($faqName);
+            $pageFileName = $faqId . ".html.twig";
+
+            // Envoi des données vers la BDD
+            $entityManager = $doctrine->getManager();
+            $faqQuestion = new FAQList();
+            $faqQuestion->setFaqName($faqName);
+            $faqQuestion->setFaqId($faqId);
+            $entityManager->persist($faqQuestion);
+            $entityManager->flush();
+
+            // Création du fichier
+            $file = fopen("../templates/webpages/faq_questions/" . $pageFileName, 'w', 'w');
+            fwrite($file, $faqContent);
+            fclose($file);
+
+            // Redirection vers la page crée
+            return $this->redirectToRoute('app_admin_sav_manager');
+        }
+
         return $this->render('admin_sav_manager/add-question.html.twig', [
             'form' => $form->createView(),
             'controller_name' => 'AdminSAVManagerController',
         ]);
-        
+    }
+
+    /* MODIFIER UNE QUESTION
+    ------------------------------------------------------- */
+    #[Route('/admin/sav-manager/modifier-question/{faq_id}', name: 'app_admin_sav_manager_modify_question')]
+    public function modify_question(ManagerRegistry $doctrine, Request $request, String $faq_id)
+    {
+
+        $form = $this->createForm(FAQManagerFormType::class);
+        $form->handleRequest($request);
+
+        // Récupération de la question souhaitée
+        $entityManager = $doctrine->getManager();
+        $question = $entityManager->getRepository(FAQList::class)->findOneBy(['faq_id' => $faq_id]);
+        $slugify = new Slugify();
+
+        if(!$question) {
+            throw $this->createNotFoundException(
+                "Aucune question n'a été trouvée"
+            );
+        }
+
+        // Récupération du contenu de la page
+        $quesContent = file_get_contents("../templates/webpages/faq_questions/" . $faq_id . ".html.twig");
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération des données du form
+            $data = $form->getData();
+            $faqName = $data['faq_name'];
+            $faqContent = $data['faq_content'];
+            $faqId = $slugify->slugify($faqName);
+            $pageFileName = $faqId . ".html.twig";
+
+            // Envoi des données vers la BDD
+            $entityManager = $doctrine->getManager();
+            $question->setFaqName($faqName);
+            $question->setFaqId($faqId);
+            $entityManager->persist($question);
+            $entityManager->flush();
+
+            // Création du fichier
+            $file = fopen("../templates/webpages/faq_questions/" . $pageFileName, 'w', 'w');
+            fwrite($file, $faqContent);
+            fclose($file);
+
+            // Redirection vers la page crée
+            return $this->redirectToRoute('app_admin_sav_manager_modify_question', ['faq_id' => $faqId]);
+        }
+
+        return $this->render('admin_sav_manager/modify-question.html.twig', [
+            'form' => $form->createView(),
+            'faqName' => $question->getFaqName(),
+            'faqContent' => $quesContent,
+            'controller_name' => 'AdminSAVManagerController',
+        ]);
+    }
+
+    /* SUPPRIMER UNE QUESTION
+    ------------------------------------------------------- */
+    #[Route('/admin/sav-manager/suppimer-question/{faq_id}', name: 'app_admin_sav_manager_delete_question')]
+    public function delete_question(ManagerRegistry $doctrine, String $faq_id)
+    {
+        // Suppression de la valeur dans la BDD
+        $entityManager = $doctrine->getManager();
+        $faqQuestion = $entityManager->getRepository(FAQList::class)->findOneBy(['faq_id' => $faq_id]);
+
+        if(!$faqQuestion) {
+            throw $this->createNotFoundException(
+                "Aucune question n'a été trouvée"
+            );
+        }
+
+        $entityManager->remove($faqQuestion);
+        $entityManager->flush();
+
+        // Suppression du fichier
+        unlink("../templates/webpages/faq_questions/" . $faq_id . ".html.twig");
+
+        return $this->redirectToRoute('app_admin_sav_manager');
     }
 }
