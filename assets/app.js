3b569a41bf517d59fda7c2927530c31a86367f60
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-11-06 22:24:15 +0100

    Yarn Prod

--- assets/app.js
+++ assets/app.js
@@ -43,11 +43,40 @@ document.addEventListener('swup:contentReplaced', function(){
 })*/
 
 
+// OPEN LAYERS
+// ------------------------------------------------------------------
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj';
+
+
+if (document.querySelector(".partners-map") != undefined) {
+  const aubLonLat = [4.389863, 44.620909];
+  const aubWebMercator = fromLonLat(aubLonLat);
+  
+  const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      center: aubWebMercator,
+      zoom: 15,
+    })
+  })
+}
+
+
+
+
 
 // Parallax
 // ------------------------------------------------------------------
 import 'parallax-image';
-
 
 
 // Header
@@ -113,6 +142,7 @@ subNavButtons.forEach(subNavButton => {
 
 
 
+
 // Contact Form
 // ------------------------------------------------------------------
 var contactButton = document.querySelectorAll('.toggle-contact');
@@ -124,44 +154,7 @@ contactButton.forEach(button => {
   }
 });
 
-var contactBtn1 = document.querySelector('#contactBtn1');
-var contactBtn2 = document.querySelector('#contactBtn2');
-var contactBtn3 = document.querySelector('#contactBtn3');
 
-console.log(document.querySelectorAll("input[name='contact_form[gender]']").length);
-
-contactBtn1.onclick = function () {
-  document.querySelector("#contact-tab-1").classList.add('active');
-  document.querySelector("#contact-tab-2").classList.remove('active');
-  document.querySelector("#contact-tab-3").classList.remove('active');
-
-  contactBtn1.classList.remove('full');
-}
-
-contactBtn2.onclick = function () {
-  if (document.querySelectorAll("input[name='contact_form[gender]']:checked").length < 1) {return false;}
-  if (document.querySelector('#contact_form_lname').value == "") {return false;}
-  if (document.querySelector('#contact_form_fname').value == "") {return false;}
-  if (document.querySelector('#contact_form_phone').value == "") {return false;}
-  if (document.querySelector('#contact_form_email').value == "") {return false;}
-
-  document.querySelector("#contact-tab-1").classList.remove('active');
-  document.querySelector("#contact-tab-2").classList.add('active');
-  document.querySelector("#contact-tab-3").classList.remove('active');
-
-  contactBtn1.classList.add('full');
-  contactBtn2.classList.remove('full');
-};
-
-contactBtn3.onclick = function () {
-  if (document.querySelectorAll("input[name='contact_form[customer_type]']:checked").length < 1) {return false;}
-
-  document.querySelector("#contact-tab-1").classList.remove('active');
-  document.querySelector("#contact-tab-2").classList.remove('active');
-  document.querySelector("#contact-tab-3").classList.add('active');
-
-  contactBtn2.classList.add('full');
-};
 
 
 
@@ -186,10 +179,6 @@ if (document.querySelector('.sav-container') != undefined) {
   tabs(partnersTabscontainer);
 }
 
-if (document.querySelector('.options-container') != undefined) {
-  var optionsContainer = document.querySelector('.options-container')
-  tabs(optionsContainer);
-}
 
 
 
@@ -220,11 +209,15 @@ import Swiper, { Pagination, Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-const swiperLastNews = new Swiper('.posts', {
+const swiperDoc = new Swiper('.product-doc-list', {
+  modules: [Navigation],
   slidesPerView: "auto",
   centeredSlides: true,
   loop: true,
-  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-doc-button-next',
+    prevEl: '.swiper-doc-button-prev',
+  },
   breakpoints: {
     1024: {
       centeredSlides: false,
@@ -233,34 +226,6 @@ const swiperLastNews = new Swiper('.posts', {
     1200: {
       centeredSlides: true,
       slidesPerView: 3,
-    }
-  }
-})
-
-const swiperDoc = new Swiper('.product-doc-list', {
-  modules: [Navigation],
-  slidesPerView: 1,
-  centeredSlides: true,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-doc-button-next',
-    prevEl: '.swiper-doc-button-prev',
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      centeredSlides: false,
-      loop: true,
-    },
-    768: {
-      slidesPerView: 2,
-      centeredSlides: false,
-      loop: true,
-    },
-    1200: {
-      slidesPerView: 3,
-      centeredSlides: true,
-      loop: true,
     }
   }
 });
@@ -286,7 +251,7 @@ const swiperLastsEvents = new Swiper('.home-timeline-container', {
 });
 
 
-const chrono = new Swiper('.chrono-swiper-1', {
+const chrono1 = new Swiper('.chrono-swiper-1', {
   modules: [Pagination],
   spaceBetween: 30,
   pagination: {
@@ -299,7 +264,45 @@ const chrono = new Swiper('.chrono-swiper-1', {
     },
     1200: {
       spaceBetween: 60,
-      slidesPerView: 2,
+      slidesPerView: 3,
+    },
+  }
+});
+
+
+const chrono2 = new Swiper('.chrono-swiper-2', {
+  modules: [Pagination],
+  spaceBetween: 30,
+  pagination: {
+    el: ".chrono-pagination-2",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1200: {
+      spaceBetween: 60,
+      slidesPerView: 3,
+    },
+  }
+});
+
+
+const chrono3 = new Swiper('.chrono-swiper-3', {
+  modules: [Pagination],
+  spaceBetween: 30,
+  pagination: {
+    el: ".chrono-pagination-3",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1200: {
+      spaceBetween: 60,
+      slidesPerView: 3,
     },
   }
 });
@@ -316,6 +319,31 @@ const swiperBlocksInfos1 = new Swiper('.slider-mobile-container', {
       clickable: true,
     },
 });
+
+
+const swiperLastsPosts = new Swiper('.posts', {
+    modules: [Pagination],
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 2,
+        centeredSlides: false,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        centeredSlides: false,
+        spaceBetween: 30,
+      }
+    }
+})
 
 
 const swiperProductsColoris = new Swiper('.slider-coloris', {
