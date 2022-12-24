a323e747e595715fdf54dea4a527a9b412bc84a2
Creative Eye Studio <29424183+tomas-ryan-lee@users.noreply.github.com>
2022-12-24 20:33:01 +0100

    Update config.js

--- public/bundles/fosckeditor/config.js
+++ public/bundles/fosckeditor/config.js
@@ -7,5 +7,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	config.extraPlugins = 'codemirror';
-	config.sourceMode = 'source';
+	config.startupMode = 'source';
 };
