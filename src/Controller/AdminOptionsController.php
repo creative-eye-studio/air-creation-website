d3f0508baace7b775568b69158b039fbecfb2927
Kévin R <rifakev@gmail.com>
2023-03-02 13:38:48 +0100

    KR - Modification d'une option

--- src/Controller/AdminOptionsController.php
+++ src/Controller/AdminOptionsController.php
@@ -13,10 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminOptionsController extends AbstractController
 {
     #[Route('/admin/options', name: 'admin_options')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
+        $options = $entityManager->getRepository(Options::class)->findAll();
+
         return $this->render('admin_options/index.html.twig', [
-            
+            'options' => $options
         ]);
     }
 
@@ -43,5 +46,50 @@ class AdminOptionsController extends AbstractController
         return $this->render('admin_options/add-option.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/admin/options/modifier/{option_id}', name: 'admin_options_modify')]
+    public function modify_option(Request $request, ManagerRegistry $doctrine, String $option_id)
+    {
+        // Récupération de l'option souhaitée
+        $entityManager = $doctrine->getManager();
+        $option = $entityManager->getRepository(Options::class)->findOneBy(['id' => $option_id]);
+        $form = $this->createForm(OptionsFormType::class, $option);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération des données
+            $option = $form->getData();
+            
+            // Envoi des données vers la BDD
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($option);
+            $entityManager->flush();
+
+            // Redirection vers la liste des options
+            return $this->redirectToRoute('admin_options');
+        }
+
+        return $this->render('admin_options/modify-option.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/options/suppr/{option_id}', name: 'admin_options_delete')]
+    public function delete_option(ManagerRegistry $doctrine, String $option_id)
+    {
+        // Récupération de l'option souhaitée
+        $entityManager = $doctrine->getManager();
+        $option = $entityManager->getRepository(Options::class)->findOneBy(['id' => $option_id]);
+
+        if (!$option)
+            throw $this->createNotFoundException(
+                "Aucune option n'a été trouvée"
+            );
+
+        $entityManager->remove($option);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_options');
     }
 }
