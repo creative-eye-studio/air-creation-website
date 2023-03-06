247437c8bfc62d3a33254c719657f46374ebd062
Kévin R <rifakev@gmail.com>
2023-03-06 18:56:57 +0100

    KR - Corrections graphiques

--- public/bundles/fosckeditor/config.js
+++ public/bundles/fosckeditor/config.js
@@ -11,7 +11,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.extraAllowedContent = 'i';
 	config.entities = false;
 	config.baseHref = '/';
-	config.contentsCss = '../build/app.fcd4190.css';
+	config.contentsCss = '../build/app.6b401e9d.css';
 	config.height = 300;
 	config.bodyClass = 'no-js header-second';
 };
