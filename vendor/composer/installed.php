d35933b94a9da9dab1958524b8ca05071bba9de3
Kévin R <rifakev@gmail.com>
2023-03-21 11:53:25 +0100

    KR - MAJ de PHP

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '01d610476ffa66d803d0952ce29e6f29e6bd5fcd',
+        'reference' => '4efede37a7a6d2098662b8941451b741e86f6651',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '01d610476ffa66d803d0952ce29e6f29e6bd5fcd',
+            'reference' => '4efede37a7a6d2098662b8941451b741e86f6651',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -577,8 +577,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache-implementation' => array(
