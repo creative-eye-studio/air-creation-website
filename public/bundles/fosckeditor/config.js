87af2fc5db75cf6803cf454f22e2f3549d39469e
Kévin R <rifakev@gmail.com>
2023-03-22 23:47:21 +0100

    KR - Gestion des galeries d'options

--- public/bundles/fosckeditor/config.js
+++ public/bundles/fosckeditor/config.js
@@ -5,7 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	config.language = 'fr';
-	config.extraPlugins = 'codemirror,filebrowser';
+	config.extraPlugins = 'codemirror,filebrowser,powrmediagallery';
 	// config.startupMode = 'source';
 	config.allowedContent = true;
 	config.extraAllowedContent = 'i';
